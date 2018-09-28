f788e931d2b91e668d332f6bac186eeba4517fd5
Rodion Kasatkin <Rodion_Kasatkin@epam.com>
2018-09-28 08:57:17 +0300

    refactroing of the project: add system Nomenclature of card

--- src/main/java/mastercard/MasterCard.java
+++ src/main/java/mastercard/MasterCard.java
@@ -1,28 +1,34 @@
 package mastercard;
 
 import paymentSystem.Card;
+import paymentSystem.GeneratorNomenclaturesTypeCard;
+import paymentSystem.NomenclatureCard;
 import utils.RandomNumbersGenerator;
 
-public class MasterCard extends Card{
+import java.util.List;
+
+public class MasterCard extends Card {
 
 	private static final int PREFIX_MASTER_FIRST_LEFT = 51;
 	private static final int PREFIX_MASTER_FIRST_RIGHT = 55;
 	private static final int PREFIX_MASTER_SECOND_LEFT = 222100;
 	private static final int PREFIX_MASTER_SECOND_RIGHT = 272099;
+	private int countNumbers;
+
+
 	private static final String TYPE_CARD = "MasterCard";
 
 
 	public MasterCard() {
+		List<NomenclatureCard> nomenclatures = GeneratorNomenclaturesTypeCard.getMasterCardNomenclatures();
+		int rnd = RandomNumbersGenerator.rnd.nextInt(nomenclatures.size());
+		NomenclatureCard nc = nomenclatures.get(rnd);
+		super.countNum = nc.getLengthCard();
+		super.prefix = nc.getPrefix();
 		super.typeCard = typeCard;
+
+
 	}
 
-	public String getPrefixPaymentSystem() {
-		String result = "";
-		if (RandomNumbersGenerator.rnd.nextBoolean()) {
-			result = ((Integer) (RandomNumbersGenerator.rnd.nextInt(PREFIX_MASTER_FIRST_RIGHT - PREFIX_MASTER_FIRST_LEFT) + PREFIX_MASTER_FIRST_LEFT)).toString();
-		} else {
-			result = ((Integer) (RandomNumbersGenerator.rnd.nextInt(PREFIX_MASTER_SECOND_RIGHT - PREFIX_MASTER_SECOND_LEFT) + PREFIX_MASTER_SECOND_LEFT)).toString();
-		}
-		return result;
-	}
+
 }
