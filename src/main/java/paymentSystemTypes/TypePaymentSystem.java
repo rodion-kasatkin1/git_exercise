f31a24b5f678e7e85c0cab942d4d254d66d959b1
Rodion Kasatkin <Rodion_Kasatkin@epam.com>
2018-09-28 10:02:09 +0300

    refactoring of produce card numbers

--- src/main/java/paymentSystemTypes/TypePaymentSystem.java
+++ src/main/java/paymentSystemTypes/TypePaymentSystem.java
@@ -1,5 +1,21 @@
 package paymentSystemTypes;
 
+import paymentSystem.GeneratorNomenclaturesTypeCard;
+import paymentSystem.NomenclatureCard;
+import utils.RandomNumbersGenerator;
+
+import java.util.List;
+
 public enum TypePaymentSystem {
+	MASTERCARD_CLASSIC {
+		public NomenclatureCard getNomenclatureCard() {
+			List<NomenclatureCard> nomenclatures = GeneratorNomenclaturesTypeCard.getMasterCardNomenclatures();
+			int rnd = RandomNumbersGenerator.rnd.nextInt(nomenclatures.size());
+			NomenclatureCard nc = nomenclatures.get(rnd);
+			return nc;
+		}
+	};
+
+	public abstract NomenclatureCard getNomenclatureCard();
 
 }
