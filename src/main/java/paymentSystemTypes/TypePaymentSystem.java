6f31ab02fc637e3645272b0c257bfcb41fee85fe
Rodion Kasatkin <Rodion_Kasatkin@epam.com>
2018-09-28 12:45:51 +0300

    add realisation of visa electron

--- src/main/java/paymentSystemTypes/TypePaymentSystem.java
+++ src/main/java/paymentSystemTypes/TypePaymentSystem.java
@@ -9,21 +9,25 @@ import java.util.List;
 public enum TypePaymentSystem {
 	MASTERCARD_CLASSIC {
 		public NomenclatureCard getNomenclatureCard() {
-			return getMastercardNomenclature(GeneratorNomenclaturesTypeCard.getMasterCardNomenclatures());
+			return getRandomCardNomenclature(GeneratorNomenclaturesTypeCard.getMasterCardNomenclatures());
 		}
 	}, MASTERCARD_ELECTRONIC {
 		public NomenclatureCard getNomenclatureCard() {
-			return getMastercardNomenclature(GeneratorNomenclaturesTypeCard.getMasterCardNomenclatures());
+			return getRandomCardNomenclature(GeneratorNomenclaturesTypeCard.getMasterCardNomenclatures());
 		}
 	}, MASTERCARD_MAESTRO{
 		public NomenclatureCard getNomenclatureCard(){
-			return getMastercardNomenclature(GeneratorNomenclaturesTypeCard.getMasterCardMaestroNomenclatures());
+			return getRandomCardNomenclature(GeneratorNomenclaturesTypeCard.getMasterCardMaestroNomenclatures());
+		}
+	},VISA_ELECTRON{
+		public NomenclatureCard getNomenclatureCard(){
+			return getRandomCardNomenclature(GeneratorNomenclaturesTypeCard.getVisaNomenclotures());
 		}
 	};
 
 	public abstract NomenclatureCard getNomenclatureCard();
 
-	private static NomenclatureCard getMastercardNomenclature(List<NomenclatureCard> nomenclatures) {
+	private static NomenclatureCard getRandomCardNomenclature(List<NomenclatureCard> nomenclatures) {
 		int rnd = RandomNumbersGenerator.rnd.nextInt(nomenclatures.size());
 		NomenclatureCard nc = nomenclatures.get(rnd);
 		return nc;
